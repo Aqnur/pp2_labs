ee68a3a1d7c6fe68196f866f387acb26148ef99d
Aknur Toktarov <aknurr999@gmail.com>
2019-02-13 20:45:15 +0600

    far manager

--- week3/far manager/Program.cs
+++ week3/far manager/Program.cs
@@ -26,13 +26,13 @@ namespace FarManager
             get;
             set;
         }
-        public void Draw()
+        public void Draw() //function to show visual part of directory
         {
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Clear();
-            for (int i = 0; i < Content.Length; ++i)
+            Console.BackgroundColor = ConsoleColor.Black; //makes background black
+            Console.Clear(); //clears
+            for (int i = 0; i < Content.Length; ++i) //loop from 0 to length of content 
             {
-                if (i == SelectedIndex)
+                if (i == SelectedIndex) //selected index is blue whiel others white
                 {
                     Console.BackgroundColor = ConsoleColor.Blue;
                 }
@@ -40,8 +40,8 @@ namespace FarManager
                 {
                     Console.BackgroundColor = ConsoleColor.Black;
                 }
-                Console.Write(i+1 + ". ");
-                Console.WriteLine(Content[i].Name);
+                Console.Write(i+1 + ". "); //The numbered order of content
+                Console.WriteLine(Content[i].Name); //output the names of content
             }
         }
 
@@ -50,9 +50,8 @@ namespace FarManager
     {
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\aknur\Desktop\CN");
-            string path = @"C:\Users\aknur\Desktop\CN";
-            Layer l = new Layer
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\aknur\Desktop\CN"); //the path
+            Layer l = new Layer //the layer
             {
                 Content = dir.GetFileSystemInfos(),
                 SelectedIndex = 0
@@ -60,44 +59,44 @@ namespace FarManager
 
             FSIMode curMode = FSIMode.DirectoryInfo;
 
-            Stack<Layer> history = new Stack<Layer>();
-            history.Push(l);
+            Stack<Layer> history = new Stack<Layer>(); //creating stack of layher
+            history.Push(l); //pushing the layer
 
-            bool esc = false;
-            while (!esc)
+            bool esc = false; //boolean false
+            while (!esc) //while func which ends if esc is false
             {
                 if (curMode == FSIMode.DirectoryInfo)
                 {
                     history.Peek().Draw();
                 }
                 ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
-                switch (consoleKeyInfo.Key)
+                switch (consoleKeyInfo.Key) //switch case to bind keys to some functions as like if func
                 {
-                    case ConsoleKey.UpArrow:
+                    case ConsoleKey.UpArrow://up arrow selected index decreases therefore it goes upwards
                         history.Peek().SelectedIndex--;
                         break;
-                    case ConsoleKey.DownArrow:
+                    case ConsoleKey.DownArrow: //same as up arrow but increase
                         history.Peek().SelectedIndex++;
                         break;
-                    case ConsoleKey.Enter:
+                    case ConsoleKey.Enter: //enter button
                         int index = history.Peek().SelectedIndex;
                         FileSystemInfo fsi = history.Peek().Content[index];
-                        if (fsi.GetType() == typeof(DirectoryInfo))
+                        if (fsi.GetType() == typeof(DirectoryInfo)) //identifyes is selected index is file or directory
                         {
                             curMode = FSIMode.DirectoryInfo;
                             DirectoryInfo d = fsi as DirectoryInfo;
-                            history.Push(new Layer
+                            history.Push(new Layer  
                             {
                                 Content = d.GetFileSystemInfos(),
                                 SelectedIndex = 0
-                            });
+                            }); //pushing new layer
                         }
                         else
                         {
-                            curMode = FSIMode.File;
+                            curMode = FSIMode.File; //if file it opens it
                             using (FileStream fs = new FileStream(fsi.FullName, FileMode.Open, FileAccess.Read))
                             {
-                                using (StreamReader sr = new StreamReader(fs))
+                                using (StreamReader sr = new StreamReader(fs)) //reads and makes chance to edit file
                                 {
                                     Console.BackgroundColor = ConsoleColor.White;
                                     Console.ForegroundColor = ConsoleColor.Black;
@@ -107,10 +106,10 @@ namespace FarManager
                             }
                         }
                         break;
-                    case ConsoleKey.Backspace:
+                    case ConsoleKey.Backspace: //back space
                         if (curMode == FSIMode.DirectoryInfo)
                         {
-                            history.Pop();
+                            history.Pop(); //just pops the layer back
                         }
                         else
                         {
@@ -118,36 +117,46 @@ namespace FarManager
                             Console.ForegroundColor = ConsoleColor.White;
                         }
                         break;
-                    case ConsoleKey.Delete:
+                    case ConsoleKey.Delete: //delete file or folder
                         int indexDel = history.Peek().SelectedIndex;
                         FileSystemInfo fsiDel = history.Peek().Content[indexDel];
-                        string pathDelete = path + @"\" + fsiDel;
-                        if(fsiDel.GetType() == typeof(DirectoryInfo))
+                        string pathDelete = dir + @"\" + fsiDel; //the path
+                        if(fsiDel.GetType() == typeof(DirectoryInfo))//finds if folder of file
                         {
-                            Directory.Delete(pathDelete);
+                            Directory.Delete(pathDelete); //delets
                         }
                         else
                         {
-                            File.Delete(pathDelete);
+                            File.Delete(pathDelete);//delets
                         }
+                        history.Push(new Layer //push as new layer
+                        {
+                            Content = dir.GetFileSystemInfos(),
+                            SelectedIndex = 0
+                        });
                         break;
-                    case ConsoleKey.F4:
+                    case ConsoleKey.F4: //rename the file or folder the same as delete
                         Console.Clear();
                         int indexReName = history.Peek().SelectedIndex;
                         FileSystemInfo fsiReName = history.Peek().Content[indexReName];
                         string pathRename = Console.ReadLine();
-                        string path1 = path + @"\" + fsiReName;
-                        string path2 = path + @"\" + pathRename;
+                        string path1 = dir + @"\" + fsiReName;
+                        string path2 = dir + @"\" + pathRename;
                         if (fsiReName.GetType() == typeof(DirectoryInfo))
                         {
-                            Directory.Move(path, path1);
+                            Directory.Move(path1, path2);
                         }
                         else
                         {
                             File.Move(path1, path2);
                         }
+                        history.Push(new Layer
+                        {
+                            Content = dir.GetFileSystemInfos(),
+                            SelectedIndex = 0
+                        });
                         break;
-                    case ConsoleKey.Escape:
+                    case ConsoleKey.Escape: //escape turns the esc true then the program stops
                         esc = true;
                         break;
                 }
