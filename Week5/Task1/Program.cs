ae4344e0d138a580f6d52a8a5da80846d769a41a
Aknur Toktarov <aknurr999@gmail.com>
2019-02-26 20:41:14 +0600

    complex

--- Week5/Task1/Program.cs
+++ Week5/Task1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Task1
 {
@@ -18,6 +19,34 @@ namespace Task1
             a = 1;
             b = 1;
         }
+
+        public ComplexNumber(int _realNum = 1, int _ImageNum = 1)
+        {
+            a = _realNum;
+            b = _ImageNum;
+        }
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            ComplexNumber c3 = new ComplexNumber();
+            c3.b = lcm(c1.b, c1.b);
+            c3.a = ((c3.b / c1.b) * c1.a) + (c2.a * (c3.b / c2.b));
+            int Gcd = gcd(c3.a, c3.b);
+            c3.a /= Gcd;
+            c3.b /= Gcd;
+            return c3;
+        }
+        public override string ToString()
+        {
+            return Convert.ToString(a) + '/' + Convert.ToString(b);
+        }
+        public static int lcm(int a, int b)
+        {
+            return (a * b) / gcd(a, b);
+        }
+        public static int gcd(int a, int b)
+        {
+            return b == 0 ? a : gcd(b, a % b);
+        }
     }
     class Program
     {
@@ -26,13 +55,13 @@ namespace Task1
             F1();
             F2();
         }
-        
+
         private static void F1()
         {
             ComplexNumber cn = new ComplexNumber();
             FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.Write);
             XmlSerializer xs = new XmlSerializer(typeof(ComplexNumber));
-
+                
             xs.Serialize(fs, cn);
             fs.Close();
         }
--- Week5/Task2/Program.cs
+++ Week5/Task2/Program.cs
@@ -10,13 +10,42 @@ namespace Task2
     [Serializable]
     public class Mark
     {
-        
+        public int n;
+        public Mark()
+        {
+
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            mark(n);
+        }
+        static void mark(int n)
+        {
+            if (n >= 0 && n <= 49)
+            {
+                Console.WriteLine("F");
+            }
+            else if (n >= 50 && n <= 74)
+            {
+                Console.WriteLine("C");
+            }
+            else if (n >= 75 && n <= 89)
+            {
+                Console.WriteLine("B");
+            }
+            else if (n >= 90 && n <= 100)
+            {
+                Console.WriteLine("A");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
         }
     }
 }
