021d44f6ce3fbc3483593d2a62e44c80652bdc8e
Aknur Toktarov <aknurr999@gmail.com>
2019-04-05 14:23:09 +0600

    calc

---
+++ Calculator/Week8/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week8
+{
+    public partial class Form1 : Form
+    {
+        double resultValue = 0;
+        string operationPerformed = "";
+        bool IsOperatorWork = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            if(textBox_result.Text == "0")
+            {
+                textBox_result.Clear();
+            }
+            IsOperatorWork = false;
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + button.Text;
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Text;
+            resultValue = double.Parse(textBox_result.Text);
+            IsOperatorWork = true;
+            textBox_result.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox_result.Text = (resultValue + double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_result.Text = (resultValue - double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_result.Text = (resultValue * double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_result.Text = (resultValue / double.Parse(textBox_result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Quiz2/Task1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    [Serializable]
+    public class Employee
+    {
+        private string name;
+        private string ID;
+        private int salary;
+        public string N
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string I
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        public int S
+        {
+            get
+            {
+                return salary += 50000;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string ID = Console.ReadLine();
+            int salary = Convert.ToInt32(Console.ReadLine());
+            Employee x = new Employee{N = name, I = ID, S = salary};
+            ser(x);
+            deser(x);
+        }
+        static Employee deser(Employee c)
+        {
+            FileStream fs = new FileStream("employee.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+
+            Employee e = xs.Deserialize(fs) as Employee;
+
+            fs.Close();
+            Console.WriteLine(e.N);
+            Console.WriteLine(e.I);
+            Console.WriteLine(e.S);
+            return e;
+        }
+
+        private static void ser(Employee c)
+        {
+            FileStream fs = new FileStream("employee.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+
+            xs.Serialize(fs, c);
+
+            fs.Close();
+        }
+    }
+}
